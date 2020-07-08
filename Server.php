85e5fc5af5489f9361c3cf3a401aa5830f8ae1b4
rocket-builder <bogdan14.228@gmail.com>
2020-07-08 19:58:46 +0300

    delete api lib

--- Server.php
+++ Server.php
@@ -1,7 +1,6 @@
 <?php
-include('vendor/autoload.php'); //Подключаем библиотеку
-  use Telegram\Bot\Api;
-  require 'Config.php';
+require 'Config.php';
+require 'utils/Utils';
 
   $file = file_get_contents('https://api.tlgr.org/bot'.Config::BOT_TOKEN.'/getUpdates');
 
