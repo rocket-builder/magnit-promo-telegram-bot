2ed5602c4b33ac328c66fc0782167fe02589fef6
rocket-builder <bogdan14.228@gmail.com>
2020-07-11 12:13:05 +0300

    final long polling demo

--- Server.php
+++ Server.php
@@ -7,14 +7,28 @@ $telegram=new Telegram(Config::BOT_TOKEN, Config::BOT_NAME);
 
 while (true) {
 
-	$upd = $telegram->getupdates(@$upd['update_id'] + 1);
+	$update = $telegram->getupdates(@$update['update_id'] + 1);
 
-  $update = (object)$upd;
-  if (isset($update->message)) {
+  if (isset($update['message'])) {
 
-      // $message = $update->message;
-      // var_dump($messsage);
-      var_dump($update);
+      switch ($update['message']['text']) {
+        case '/start':
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => 'Привет'
+          ]);
+          break;
+
+        default:
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => 'Как дела, это новый кадилак'
+          ]);
+          break;
+      }
+
   }
 }
 ?>
