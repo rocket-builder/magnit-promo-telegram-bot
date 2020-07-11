a85ca785528490d030bda49ee703d4464500d884
rocket-builder <bogdan14.228@gmail.com>
2020-07-11 15:09:43 +0300

    work on cell promo functional

--- Server.php
+++ Server.php
@@ -3,99 +3,110 @@ require 'Config.php';
 require 'utils/API.php';
 require 'controllers/connection.php';
 
-$card = R::load('card',1);
 
-$customer = R::dispense('customer');
-$customer->name = '1213322';
-$customer->balance = 12.23;
-R::store($customer);
-
-$orders = R::dispense('orders');
-$orders->card = $card;
-$orders->customer = $customer;
-$orders->date = date('Y-m-d');
-R::store($orders);
+$keyboard = [
+    ['Купить', 'Наличие товаров'],
+    ['Баланс', 'Личный кабинет'],
+    ['Помощь', 'Правила', 'О боте']
+];
 
 
+$telegram = new Telegram(Config::BOT_TOKEN, Config::BOT_NAME);
 
-// $keyboard = [
-//     ['Купить', 'Наличие товаров'],
-//     ['Баланс', 'Личный кабинет'],
-//     ['Помощь', 'Правила', 'О боте']
-// ];
-//
-//
-// $telegram = new Telegram(Config::BOT_TOKEN, Config::BOT_NAME);
-//
-// while (true) {
-//
-// 	$update = $telegram->getupdates(@$update['update_id'] + 1);
-//
-//   if (isset($update['message'])) {
-//
-//       switch ($update['message']['text']) {
-//         case '/start':
-//
-//                $resp = array("keyboard" => $keyboard,"resize_keyboard" => true,"one_time_keyboard" => false);
-//                $reply = json_encode($resp);
-//
-//            $telegram->api("sendMessage", array(
-//               'chat_id' => $update['message']['chat']['id'],
-//               'text' => 'Привет!',
-//               'reply_markup' => $reply
-//            ));
-//           break;
-//
-//         case 'Купить':
-//
-//
-//           break;
-//
-//         case 'Наличие товаров':
-//
-//           break;
-//
-//         case 'Баланс':
-//
-//           break;
-//
-//         case 'Личный кабинет':
-//
-//           break;
-//
-//         case 'Помощь':
-//
-//           $telegram->api('sendMessage', [
-//             'chat_id' => $update['message']['chat']['id'],
-//             'text' => Config::HELP
-//           ]);
-//           break;
-//
-//         case 'Правила':
-//
-//           $telegram->api('sendMessage', [
-//             'chat_id' => $update['message']['chat']['id'],
-//             'text' => Config::RULES
-//           ]);
-//           break;
-//
-//         case 'О боте':
-//
-//           $telegram->api('sendMessage', [
-//             'chat_id' => $update['message']['chat']['id'],
-//             'text' => Config::ABOUT
-//           ]);
-//           break;
-//
-//         default:
-//
-//           $telegram->api('sendMessage', [
-//             'chat_id' => $update['message']['chat']['id'],
-//             'text' => 'Я не знаю такой команды :('
-//           ]);
-//           break;
-//       }
-//
-//   }
-// }
+while (true) {
+
+	$update = $telegram->getupdates(@$update['update_id'] + 1);
+
+  if (isset($update['message'])) {
+
+      switch ($update['message']['text']) {
+        case '/start':
+
+           $telegram->api("sendMessage", array(
+              'chat_id' => $update['message']['chat']['id'],
+              'text' => 'Привет!',
+              'reply_markup' => json_encode([
+                "keyboard" => $keyboard,
+                "resize_keyboard" => true,
+                "one_time_keyboard" => false
+              ])
+           ));
+          break;
+
+        case 'Купить':
+
+          $regions = R::getAll('select title from region inner join promo on region.id = promo.region_id and promo.use_date is null group by title');
+
+          $rg_count = count($regions);
+          if($rg_count > 0) {
+
+            $keys = [];
+            foreach ($regions as $region) {
+              array_push($keys, $region['title']);
+            }
+            $keys[$rg_count] = 'Главное меню';
+
+            $telegram->api("sendMessage", array(
+               'chat_id' => $update['message']['chat']['id'],
+               'text' => 'Выберете регион',
+               'reply_markup' => json_encode([
+                 "keyboard" => $keys,
+                 "resize_keyboard" => true,
+                 "one_time_keyboard" => false
+               ])
+            ));
+          } else
+            $telegram->api('sendMessage', [
+              'chat_id' => $update['message']['chat']['id'],
+              'text' => 'Промокодов пока нет в наличии, но они обязательно появятся очень скоро :)'
+            ]);
+          break;
+
+        case 'Наличие товаров':
+
+          break;
+
+        case 'Баланс':
+
+          break;
+
+        case 'Личный кабинет':
+
+          break;
+
+        case 'Помощь':
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => Config::HELP
+          ]);
+          break;
+
+        case 'Правила':
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => Config::RULES
+          ]);
+          break;
+
+        case 'О боте':
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => Config::ABOUT
+          ]);
+          break;
+
+        default:
+
+          $telegram->api('sendMessage', [
+            'chat_id' => $update['message']['chat']['id'],
+            'text' => 'Я не знаю такой команды :('
+          ]);
+          break;
+      }
+
+  }
+}
 ?>
